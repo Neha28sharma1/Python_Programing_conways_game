0734f1346cb7721c93b69b75dba2a37b38e40e47
Neha Sharma <neha28.sharma1@gmail.com>
2026-09-17 10:05:33 +0200

    fixed the type erros in functions and test

--- main/functions.py
+++ main/functions.py
@@ -1,63 +1,83 @@
-
 def create_grid(rows: int, cols: int) -> list[list[bool]]:
-    grid = [] # main grid 
+    grid: list[list[bool]] = []  # main grid
     for _ in range(rows):
-        new_row = [] # grid for each row 
+        new_row: list[bool] = []  # grid for each row
         for _ in range(cols):
-            new_row.append(False)  # Initialize each cell with bool false 
+            new_row.append(False)  # Initialize each cell with bool false
         grid.append(new_row)
     return grid
-    
-    
+
+
 def count_neighbors(grid: list[list[bool]], row: int, col: int) -> int:
     counter = 0
-    
-    for row_steps in [-1, 0, 1]: # -1 for row above , 0 for same row and +1 for row down 
-        for col_steps in [-1, 0, 1]: # -1 for col above , 0 for same col and +1 for col down 
+
+    for row_steps in [
+        -1,
+        0,
+        1,
+    ]:  # -1 for row above , 0 for same row and +1 for row down
+        for col_steps in [
+            -1,
+            0,
+            1,
+        ]:  # -1 for col above , 0 for same col and +1 for col down
             neighbor_row = row + row_steps
             neighbor_col = col + col_steps
-            
-            if neighbor_row == row and neighbor_col == col: # we dnt want to count the current cell as neighbor 
+
+            if (
+                neighbor_row == row and neighbor_col == col
+            ):  # we dnt want to count the current cell as neighbor
                 continue  # Skip the cell itself
-            if neighbor_row < 0 or neighbor_row >= len(grid) or neighbor_col < 0 or neighbor_col >= len(grid[0]):
+            if (
+                neighbor_row < 0
+                or neighbor_row >= len(grid)
+                or neighbor_col < 0
+                or neighbor_col >= len(grid[0])
+            ):
                 continue
-            if grid[neighbor_row][neighbor_col] == True:
+            if grid[neighbor_row][neighbor_col]:
                 counter = counter + 1
-    return counter  
-            
+    return counter
+
+
 def next_generation(grid: list[list[bool]]) -> list[list[bool]]:
-    
-    new_grid = create_grid(len(grid), len(grid[0]))  #grid for the next generation
-    
-    for row in range(len(grid)): #Go through the positions of all the rows in grid, one by one
-        for col in range(len(grid[0])): #grid[0] represents the first row and we want to know how many col are there in the row 
-            cell = grid[row][col]  # to get the value of one specific cell from the grid so that we can later check if it is true or false 
-            neighbors = count_neighbors(grid,row,col)
-            
-            if cell == True and neighbors < 2:  # rules of Conway’s Game of Life 
+    new_grid = create_grid(len(grid), len(grid[0]))  # grid for the next generation
+
+    for row in range(
+        len(grid)
+    ):  # Go through the positions of all the rows in grid, one by one
+        for col in range(
+            len(grid[0])
+        ):  # grid[0] represents the first row and we want to know how many col are there in the row
+            cell = grid[
+                row
+            ][
+                col
+            ]  # to get the value of one specific cell from the grid so that we can later check if it is true or false
+            neighbors = count_neighbors(grid, row, col)
+
+            if cell and neighbors < 2:  # rules of Conway’s Game of Life
                 cell = False
-            elif cell == True and (neighbors == 2 or neighbors == 3):
+            elif cell and (neighbors == 2 or neighbors == 3):
                 cell = True
-            elif cell == True and neighbors >3:
+            elif cell and neighbors > 3:
                 cell = False
-            elif cell == False and neighbors == 3:
+            elif not cell and neighbors == 3:
                 cell = True
-            new_grid[row][col] = cell # replace the current value of the cell and adds the new value in new grid
-                
+            new_grid[row][col] = (
+                cell  # replace the current value of the cell and adds the new value in new grid
+            )
+
     return new_grid
 
 
 def display_grid(grid: list[list[bool]]) -> None:
     for row in grid:
         for cell in row:
-            if cell == True:
+            if cell:
                 print("@", end="")
             else:
                 print("*", end="")
 
         # Move to the next line after printing the entire row
         print()
-                    
-                
-            
-                
--- main/test_.py
+++ main/test_.py
@@ -1,32 +1,35 @@
+from functions import create_grid, count_neighbors, next_generation
 
-from functions import create_grid,count_neighbors,next_generation
 
 def test_create_grid():
-    grid = create_grid(3,4)
-    
-    expected_grid = [[False,False,False,False],
-                     [False,False,False,False],
-                     [False,False,False,False]]
+    grid = create_grid(3, 4)
+
+    expected_grid = [
+        [False, False, False, False],
+        [False, False, False, False],
+        [False, False, False, False],
+    ]
     assert grid == expected_grid
-    
+
+
 def test_count_neighbors():
-   
-    grid = [[0,0,0],
-            [1,1,1],
-            [0,0,1]]
-    
-    count = count_neighbors(grid,1,1)
+    grid = [[False, False, False], [True, True, True], [False, False, True]]
+
+    count = count_neighbors(grid, 1, 1)
     assert count == 3
-    
+
+
 def test_next_generation():
-    grid = [[0,0,0,0],
-            [1,0,0,0],
-            [1,1,1,1]]
-    
-    newgrid=next_generation(grid)
-    
+    grid = [
+        [False, False, False, False],
+        [True, False, False, False],
+        [True, True, True, True],
+    ]
+
+    newgrid = next_generation(grid)
+
     assert newgrid == [
-    [0, 0, 0, 0],
-    [1, 0, 1, 0],
-    [1, 1, 1, 0],
+        [False, False, False, False],
+        [True, False, True, False],
+        [True, True, True, False],
     ]
