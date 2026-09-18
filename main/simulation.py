1603854c00b935f5a96a3d1f63492e741d9abde7
PJASH <ashlinapj2001@gmail.com>
2026-09-18 15:02:56 +0200

    global statistics calculation

--- main/simulation.py
+++ main/simulation.py
@@ -4,7 +4,7 @@ import msvcrt
 import random
 from functions import next_generation,display_grid
 
-def generate_random_grid(rows:int, cols:int, density:float=0.3):
+def generate_random_grid(rows: int, cols: int, density: float=0.3):
     with open("glider.txt","w") as file:
         for _ in range(rows):
             row=""
@@ -18,32 +18,55 @@ def generate_random_grid(rows:int, cols:int, density:float=0.3):
             file.write(row+"\n")
     print("Random World Generated and saved to the txt file.")
 
-def parse_grid(filename:str) -> list[list[bool]]:
-     # Read each line, remove the newline character,and convert the line into a list of characters
-    rows=cols=random.randint(10,50)
+def parse_grid(filename: str) -> list[list[bool]]:
+    # Create random integer in range 10 to 20 for the rows and cols
+    rows=cols=random.randint(6,10)
     generate_random_grid(rows,cols)
+
     with open(filename, "r") as file:
         # Convert @ to True and * to False for each cell
         grid = [[cell == "@" for cell in line.strip()]
             for line in file]
     return grid
 
+def global_statistics(current_grid: list[list[bool]],next_grid: list[list[bool]]) ->None:
+    living_cells=0
+    births=0
+    deaths=0
+    for row in range(len(current_grid)):
+        for col in range(len(current_grid[0])):
+            current_cell=current_grid[row][col]
+            next_cell=next_grid[row][col]
+            if current_cell:
+                living_cells+=1
+            if not current_cell and next_cell:
+                births+=1
+            elif not next_cell and current_cell:
+                deaths+=1
+    print("Number of living cells: ",living_cells)
+    print("Births in the current generation: ",births)
+    print("Deaths in the current generation: ",deaths)
+            
 def run_simulation(filename: str, generations: int) -> None:
 
-    grids = parse_grid(filename)
+    grid = parse_grid(filename)
     for generation in range(generations):
         # Clear the terminal
         subprocess.run("cls", shell=True)       
         # Print the generation number
         print(f"Generation {generation+1}")
-        display_grid(grids)       
-        # Wait for one second
+        # Display the grid
+        display_grid(grid)       
+        # Wait for 0.5 second
         time.sleep(0.5)
+
         # Check if a key has been pressed
         if msvcrt.kbhit():
             msvcrt.getch()
             print("\nSimulation terminated by user.")
             return
         # Generate the next grid
-        grids = next_generation(grids)
+        next_grid = next_generation(grid)
+        global_statistics(grid,next_grid)
+        grid=next_grid
 
